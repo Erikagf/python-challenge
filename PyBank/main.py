66b4dac6d998bc11ec3670ca1e7d9fc6d61609cf
Erika Flores <egflores9@gmail.com>
2019-07-06 20:08:46 -0700

    added results and changes

--- PyBank/main.py
+++ PyBank/main.py
@@ -39,15 +39,25 @@ with open(budget_csv, newline="") as csvfile:
         if(total_months.count(row[0]) == 0):
             total_months.append(row[0])
         
-
+    print("Financial Analysis")
+    print("------------------")
     print(f"Total Months: {len(total_months)}")
     print(f"Total: ${net_total}")
     print(f"Average Change: ${round(avg_change/avg_month, 2)}")
     print(f"Greatest Increase in Profits: {profit_m} (${profit})")
     print(f"Greatest Decrease in Profits: {loss_m} (${loss})")
 
+#r stands for carraige return and n stands for new line, its like alt-ent in excel for a cell
 
-
+f = open("results.txt", "w")
+f.write("Financial Analysis\r\n")
+f.write("------------------\r\n")
+f.write(f"Total Months: {len(total_months)}\r\n")
+f.write(f"Total: ${net_total}\r\n")
+f.write(f"Average Change: ${round(avg_change/avg_month, 2)}\r\n")
+f.write(f"Greatest Increase in Profits: {profit_m} (${profit})\r\n")
+f.write(f"Greatest Decrease in Profits: {loss_m} (${loss})\r\n")
+f.close()
 
 
 
