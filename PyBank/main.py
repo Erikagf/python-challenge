223f72cce04ba1f5d7ed96dfdef7e355fdee1de2
Erika Flores <egflores9@gmail.com>
2019-07-06 19:56:46 -0700

    hello world

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,69 @@
+import os
+import csv
+budget_csv = ('budget_data.csv')
 
+total_months = []
+net_total = 0
+avg_change = 0
+lastamount = 0
+avg_month = 0
+profit = 0
+loss = 0
+profit_m = ""
+loss_m = ""
+
+
+with open(budget_csv, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    next(csvreader)
+    for row in csvreader:
+        net_total += int(row[1])
+
+        if(lastamount != 0):
+            avg_change = avg_change + (int(row[1]) - lastamount)
+            avg_month = avg_month + 1
+
+            if(((int(row[1]) - lastamount) > 0) & (profit < (int(row[1]) - lastamount)) ):
+                profit = (int(row[1]) - lastamount)
+                profit_m = row[0]
+
+
+            
+            if(((int(row[1]) - lastamount) < 0)  &  (loss > (int(row[1]) - lastamount)) ):
+                loss  = (int(row[1]) - lastamount)
+                loss_m = row[0]
+
+        lastamount = int(row[1])
+        
+
+        if(total_months.count(row[0]) == 0):
+            total_months.append(row[0])
+        
+
+    print(f"Total Months: {len(total_months)}")
+    print(f"Total: ${net_total}")
+    print(f"Average Change: ${round(avg_change/avg_month, 2)}")
+    print(f"Greatest Increase in Profits: {profit_m} (${profit})")
+    print(f"Greatest Decrease in Profits: {loss_m} (${loss})")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+  
