2743a9f75e207b60dfef9060221f4d58765e1450
Erika Flores <egflores9@gmail.com>
2019-07-06 21:41:32 -0700

    adding files

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,40 @@
+import os
+import csv
+election_csv = ('election_data.csv')
 
+polls = []
+names = []
+total = 0
+winner = 0
+winner_name = ""
+current_count = 0
+
+with open(election_csv, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    next(csvreader)
+    for row in csvreader:
+        if(names.count(row[2]) == 0):
+            names.append(row[2])
+        
+        polls.append(row[2])
+
+
+
+
+
+print("Election Results")
+print("------------------")
+for name in names:
+    current_count = polls.count(name)
+    total = total + current_count
+    if winner < current_count:
+        winner = current_count
+        winner_name = name
+print(f"Total Votes: {total}")
+print("------------------")
+for name in names:
+    print(f"{name}: {round(((polls.count(name)/total)*100), 3)}% ({polls.count(name)})")
+
+print("------------------")
+print(f"Winner: {winner_name}")
+print("------------------")
